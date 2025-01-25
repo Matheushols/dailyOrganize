34c44f74137dba41ec1e510ff679eeac9cb56e33
matheushols <matheusholls@hotmail.com>
2025-01-25 17:39:30 -0300

    stablished conection with data base

--- database/data-base.go
+++ database/data-base.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
 func main() {
+	stringConnectioUrl := "root:12345678@/dailyorganize?charset=utf8&parseTime=True&loc=Local"
+	db, erro := sql.Open("mysql", stringConnectioUrl)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer db.Close()
+
+	if erro = db.Ping(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println("Conected on data base.")
 
 }
